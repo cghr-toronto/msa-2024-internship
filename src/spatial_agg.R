0b36dc4251b7274068192175de079e1f2831970e
Dante Christopher-Alphonso <dantealphonso@gmail.com>
2024-03-22 14:37:38 -0400

    added changes

--- src/spatial_agg.R
+++ src/spatial_agg.R
@@ -4,13 +4,13 @@ library(ggplot2)
 
 ## Read data
 # Reading in the Neighbourhoods GeoJson File and reprojecting
-ngh <- st_read("...experiments/data/Neighbourhoods.geojson") %>% st_transform(3857)
+ngh <- st_read("experiments/data/Neighbourhoods.geojson") %>% st_transform(3857)
 
 # Reading in the Neighbourhood Improvement Areas GeoJson File and reprojecting
-ngh_imp <- st_read("...experiments/data/Neighbourhood Improvement Areas.geojson") %>% st_transform(3857)
+ngh_imp <- st_read("experiments/data/Neighbourhood Improvement Areas.geojson") %>% st_transform(3857)
 
 # Creating spatial_agg function
-spatial_agg <- function(gdf, gdf_agg, id_col, mappings){
+spatial_agg <- function(gdf, gdf_agg, gdf_agg_id, mappings){
 
   # Join gdf and gdf_agg
   sjoin_gdf = st_join(gdf, gdf_agg)
@@ -18,22 +18,18 @@ spatial_agg <- function(gdf, gdf_agg, id_col, mappings){
   # Group the spatial joins
   grouped_sjoin = group_by(sjoin_gdf, gdf_agg_id)
   
-  # Set mappings
-  mappings <- list(
-    list(
-      column = "cause",
-      how = c("count", "mode", "mean", "sum"),
-      is_cat = TRUE
-   )
-  )
-  
 }
+
+mapping <- data.frame(
+  column = c("cause"),
+  can_aggregate = c("count,mode,mean") 
+)
 
 # Apply function
 out <- spatial_agg(
-  gdf,
-  gdf_agg,
-  id_col = "id",
+  ngh,
+  ngh_imp,
+  ngh_imp = "X_id",
   mappings = mappings
 )
 
