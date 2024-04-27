99d237058ff24901b15ff30519347c187ed090ff
Dante Christopher-Alphonso <dantealphonso@gmail.com>
2024-04-26 20:18:52 -0400

    removed summary

--- experiments/Summarise.R
+++ experiments/Summarise.R
@@ -21,6 +21,6 @@ for (func in agg_funcs) {
   # Fill your list where each key is func and each value is the columns having the relevant aggregate function
   iris_funcs[[func]] <- iris_data %>%
     filter(str_detect(can_aggregate, func)) %>%
-    pull(column)
+    pull(column)%>% summarise(var(column), .funs == func)
 }
 
