550832528d255134b46b1811f1874edd5e666cbc
Dante Christopher-Alphonso <dantealphonso@gmail.com>
2024-04-29 23:11:30 -0400

    edited iris_data

--- experiments/Summarise.R
+++ experiments/Summarise.R
@@ -9,7 +9,7 @@ df <- iris
 
 iris_data <- data.frame(
   column = c("Sepal.Length", "Sepal.Width"),
-  can_aggregate = c("sum", "mean") 
+  can_aggregate = c("sum, mean", "mean, count") 
 )
 
 agg_funcs <- c("mean", "sum", "count")
@@ -21,5 +21,7 @@ for (func in agg_funcs) {
   # Fill your list where each key is func and each value is the columns having the relevant aggregate function
   iris_funcs[[func]] <- iris_data %>%
     filter(str_detect(can_aggregate, func)) %>%
-    pull(column) %>% 
+    pull(column)
 }
+
+iris_funcs[["count"]]
