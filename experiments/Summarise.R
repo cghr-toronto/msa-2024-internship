35128c87135fdb0d4d2a1b316926edd275b7861f
Dante Christopher-Alphonso <dantealphonso@gmail.com>
2024-05-03 16:16:49 -0400

    removed count

--- experiments/Summarise.R
+++ experiments/Summarise.R
@@ -3,31 +3,28 @@ library(magrittr)
 library(sf)
 library(tidyverse)
 
-
-
 df <- iris
 
 iris_data <- data.frame(
   column = c("Sepal.Length", "Sepal.Width"),
-  can_aggregate = c("sum, mean", "mean, count") 
+  can_aggregate = c("sum, mean", "mean, median") 
 )
 
-agg_funcs <- c("mean", "sum", "count")
-
+agg_funcs <- c("mean", "sum", "median")
 
 iris_funcs <- list()
 
-for (func in agg_funcs) {
+for (func_name in agg_funcs) {
   
-  if (func %in% c("count", "mean", "sum")){
+  if (func_name %in% c("median", "mean", "sum")){
     
     # Fill your list where each key is func and each value is the columns having the relevant aggregate function
-    iris_funcs[[func]] <- iris_data %>%
-      filter(str_detect(can_aggregate, func)) %>%
+    iris_funcs[[func_name]] <- iris_data %>%
+      filter(str_detect(can_aggregate, func_name)) %>%
       pull(column)
     
-    func_name <- get(func)
+    func <- get(func_name)
     
-    sum_fun <- summarise_at(iris, iris_funcs[[func]], func_name)
+    sum_func <- summarise_at(iris, iris_funcs[[func_name]], func)
   }
 }
