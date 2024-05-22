6235468774852a2ae7bb1d879082b44e14bfc85e
DAlphonso <dantealphonso@gmail.com>
2024-05-22 12:40:57 -0400

    edited join

--- src/spatial_agg.R
+++ src/spatial_agg.R
@@ -36,46 +36,54 @@ mappings <- data.frame(
   can_aggregate = c("count,mode", "sum,median,mean,min,max") 
 )
 
-
-
-## Creating Spatial Aggregation function
-#' Title
+#' Title: Spatial Aggregation function
 #'
-#' @param gdf 
-#' @param gdf_agg 
-#' @param gdf_join 
-#' @param gdf_agg_join 
-#' @param gdf_agg_id 
-#' @param mapping 
+#' @param gdf: a sf geodataframe or data.frame containing columns with values to be aggregated 
+#' that can be spatially or non-spatially joined to gdf_agg.
+#' 
+#' @param gdf_agg: a sf geodataframe containing geometries that can be spatially joined to gdf.
+#' 
+#' @param gdf_join the column name (char) containing values in gdf to be joined non-spatially 
+#' to gdf_agg. Only applicable if is_spatial_join = TRUE. Default should be NULL.
+#' 
+#' @param gdf_agg_join: the column name (char) containing values in gdf_agg to be joined non-spatially 
+#' to gdf. Only applicable if is_spatial_join = TRUE. Default should be NULL.
+#' 
+#' @param gdf_agg_id: the column name (char) containing identifiers for rows in gdf_agg.
+#' 
+#' @param mapping: a data.frame containing the behaviour of the spatial aggregation pipeline, where each column is structured as follows:
+#'  column: the name (char) of the column in gdf to be aggregated
+#'  can_aggregate: a comma separated text (char) of aggregation functions to apply to the column (e.g. count,sum,mean,max,median,min,stdev,mode). Empty values mean this column is skipped. 
+#' 
 #' @param is_spatial_join 
 #'
-#' @return
+#' @return final_results
 #' @export
 #'
 #' @examples
 spatial_agg <- function(gdf, gdf_agg, gdf_join, gdf_agg_join, 
-                        gdf_agg_id, mapping, is_spatial_join){
+                        gdf_agg_id, mapping, is_spatial_join, ...){
   
   # Perform joins
   if (is_spatial_join == TRUE){
     
     # Spatial join
     join_gdf <- gdf %>% 
-      st_join(gdf, gdf_agg)
+      st_join(gdf, gdf_agg, ...)
     
   } else {
     
     # Non spatial join
     join_gdf <-
-      left_join(gdf, gdf_agg, by = setNames(gdf_agg_join, gdf_join))
+      left_join(gdf, gdf_agg, by = setNames(gdf_agg_join, gdf_join), ...)
   }
   
-  
+   
   # Group the joined dataframe
   group_gdf <- join_gdf %>% group_by(.data[[gdf_agg_id]])
   
   # List of aggregation functions available to use
-  agg_funcs <- c("mean", "sum", "mode")
+  agg_funcs <- c("mean", "sum", "mode", "median", "min", "max")
  
   # List of mappings functions that match with the joined gdf
   mappings_funcs <- list()
@@ -87,7 +95,7 @@ spatial_agg <- function(gdf, gdf_agg, gdf_join, gdf_agg_join,
   # Performing aggregation for columns in mappings
   for (func_name in agg_funcs) {
     
-    if (func_name %in% c("mode", "mean", "sum")){
+    if (func_name %in% c("mode", "mean", "sum", "median", "min", "max")){
       
       # Retrieve aggregation function connected to corresponding column
       mappings_funcs[[func_name]] <- mappings %>%
@@ -119,7 +127,7 @@ spatial_agg <- function(gdf, gdf_agg, gdf_join, gdf_agg_join,
       }
       
     }
-    
+   
     # Rename aggregation results columns
     agg_list[[func_name]] <- agg_list[[func_name]] %>%
       rename_with(
@@ -132,7 +140,7 @@ spatial_agg <- function(gdf, gdf_agg, gdf_join, gdf_agg_join,
   agg_results <- agg_list %>% reduce(left_join, by = gdf_agg_id)   
   
   # Joining function results back to district boundaries
-  final_results <- left_join(dist, adult_cod, by = setNames(gdf_join, gdf_agg_join))
+  final_results <- left_join(gdf_agg, gdf, by = setNames(gdf_join, gdf_agg_join))
   
   return(final_results)
   
@@ -147,4 +155,3 @@ adult_cod <- spatial_agg(gdf = adult_gid,
                          gdf_agg_id = "gid_dist",
                          mapping = mappings,
                          is_spatial_join = FALSE)
-
