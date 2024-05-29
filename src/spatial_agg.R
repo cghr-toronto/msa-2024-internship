8575e1db69a3bb75808133e749db12fb91e53732
Richard Wen <rrwen.dev@gmail.com>
2024-05-29 03:41:11 -0400

    Fix count to set empty str to NA, get avail funcs instead of agg_list for loop for spatial_agg

--- src/spatial_agg.R
+++ src/spatial_agg.R
@@ -37,21 +37,23 @@ calc_mode <- function(x, first = T, ...) {
 #' Spatially aggregates a geodataframe or dataframe to the geometries of another geodataframe.
 #'
 #' @param gdf A sf geodataframe containing geometries that can be spatially joined to gdf.
-#' @param gdf_agg A sf geodataframe or data.frame containing columns with values to be aggregated that can be spatially or non-spatially joined to gdf_agg.
-#' @param mapping A data.frame containing the behaviour of the spatial aggregation pipeline, where each column is structured as follows:
+#' @param agg A sf geodataframe or data.frame containing columns with values to be aggregated that can be spatially or non-spatially joined to agg.
+#' @param mapping A data.frame containing the behavior of the spatial aggregation pipeline, where each column is structured as follows:
 #' \itemize{
 #'    \item column: the name (char) of the column in gdf to be aggregated
 #'    \item can_aggregate: a comma separated text (char) of aggregation functions to apply to the column (e.g. count,sum,mean,max,median,min,sd,mode,var). Empty values mean this column is skipped.
 #' }
 #' @param gdf_id Column name (char) containing identifiers for rows in `gdf`. Default is `id`.
-#' @param gdf_agg_id Column name (char) containing identifiers for rows in `gdf_agg`. Default is `id`.
-#' @param is_spatial_join Set to `TRUE` to perform a spatial join using gdf_geom and gdf_agg_geom, and FALSE to perform a non-spatial join using `gdf_join` and `gdf_agg_join` columns. Default is TRUE.
-#' @param has_count Set to `TRUE` to include counts for each row in `gdf_agg` joined to `gdf` and `FALSE` to exclude.
-#' @param count_col Column name (char) containing the counts for each row in `gdf_agg` joined to `gdf`.
+#' @param agg_id Column name (char) containing identifiers for rows in `agg`. Default is `id`.
+#' @param mapping_col Column name (char) of the column in `mapping` containing column names to aggregate.
+#' @param mapping_agg_col Column name (char) of the column in `mapping` containing the comma separated function names to apply to the respective column.
+#' @param count_col Column name (char) containing the counts for each row in `agg` joined to `gdf`.
+#' @param has_count Set to `TRUE` to include counts for each row in `agg` joined to `gdf` and `FALSE` to exclude.
+#' @param is_spatial_join Set to `TRUE` to perform a spatial join, and FALSE to perform a non-spatial join using `gdf_id` and `agg_id` columns. Default is TRUE.
 #' @param agg_funcs list of aggregation functions where the key is the name matching functions in the `can_aggregate` column in `mapping`, and the value is the function itself. The `count` key is reserved to count unique values in a column.
 #' @param ... Additional arguments passed to the `st_join` (if is_spatial_join is TRUE) or `join` (otherwise) function that you will use for the aggregation.
 #'
-#' @return `gdf` with the processed aggregated results of `gdf_agg`.
+#' @return `gdf` with the processed aggregated results of `agg`.
 #'
 #' @author Dante Christopher-Alphonso, \email{adam.christopher@@torontomu.ca} and Richard Wen \email{rrwen.dev@@gmail.com}
 #' @export
@@ -60,13 +62,12 @@ calc_mode <- function(x, first = T, ...) {
 #'
 spatial_agg <- function(
     gdf,
-    gdf_agg,
+    agg,
     mapping,
     gdf_id = "id",
-    gdf_agg_id = "id",
-    is_spatial_join = TRUE,
-    has_count = TRUE,
-    count_col = "count",
+    agg_id = "id",
+    mapping_col = "column",
+    mapping_agg_col = "can_aggregate",
     agg_funcs = list(
         mean = ~ mean(.x, na.rm = TRUE),
         sum = ~ sum(.x, na.rm = TRUE),
@@ -75,29 +76,39 @@ spatial_agg <- function(
         min = ~ min(.x, na.rm = TRUE),
         max = ~ max(.x, na.rm = TRUE),
         sd = ~ sd(.x, na.rm = TRUE),
-        var = ~ var(.x, na.rm = TRUE),
-        count = ~count(.x, na.rm = TRUE)
+        var = ~ var(.x, na.rm = TRUE)
     ),
+    count_col = "count",
+    has_count = TRUE,
+    is_spatial_join = TRUE,
     ...
 ) {
+    
+    # Rename mapping col names
+    mapping <- mapping %>%
+        rename(
+            !!column := mapping_col,
+            !!can_aggregate := mapping_agg_col
+        )
     
     # Perform joins
     if (is_spatial_join == TRUE) {
         
         # Spatial join
-        join_gdf <- gdf_agg %>% st_join(gdf, ...)
+        join_gdf <- agg %>% st_join(gdf, ...)
         
     } else {
         
         # Non spatial join
         join_gdf <- gdf %>% right_join(
-            gdf_agg,
-            by = setNames(gdf_agg_id, gdf_id),
+            agg,
+            by = setNames(agg_id, gdf_id),
             ...
         )
+        
     }
     
-    # Remove geometry
+    # Remove geometry and convert to df
     join_gdf <- join_gdf %>%
         as_tibble() %>%
         select(-geometry)
@@ -106,25 +117,25 @@ spatial_agg <- function(
     group_gdf <- join_gdf %>%
         group_by(.data[[gdf_id]])
         
-    # Agg results
-    agg_list <- list()
+    # Get all available funcs from mapping
+    func_avail <- mapping %>%
+        filter(!is.na(can_aggregate)) %>%
+        mutate(
+            can_aggregate = str_remove_all(can_aggregate, " ")
+        ) %>%
+        separate_longer_delim(can_aggregate, ",") %>%
+        pull(can_aggregate) %>%
+        unique
     
-    # Performing aggregation for columns in mapping
-    for (func_name in names(agg_funcs)) {
+    # Perform aggregation for columns based on mapping
+    agg_list <- list()
+    for (func_name in names(func_avail)) {
         
         # Retrieve agg func names for column
         agg_cols <- mapping %>%
             filter(str_detect(can_aggregate, func_name)) %>%
             filter(column != gdf_id) %>%
             pull(column)
-        
-        if (length(agg_cols) == 0) {
-            next
-        }
-        
-        # Get func from func name
-        func <- agg_funcs[[func_name]]
-        
         
         # Apply functions
         if (func_name == "count") {
@@ -134,17 +145,24 @@ spatial_agg <- function(
                 agg_cols,
                 function (x)
                     group_gdf %>%
+                    mutate(!!x := na_if(.data[[x]], "")) %>%
                     count(.data[[x]]) %>%
-                    pivot_wider(names_from = x,
-                                values_from = n) %>%
+                    pivot_wider(
+                        names_from = x,
+                        values_from = n
+                    ) %>%
                     rename_with(
                         .fn = ~ paste0(x, "_", .),
                         .cols = -all_of(gdf_id)
-                    )) %>%
+                    )
+                ) %>%
                 reduce(left_join, by = gdf_id) %>%
                 replace(is.na(.), 0)
             
         } else {
+            
+            # Get func from func name
+            func <- agg_funcs[[func_name]]
           
             # Apply other functions
             agg_list[[func_name]] <- group_gdf %>%
@@ -233,9 +251,9 @@ mapping <- data.frame(
 
 # Testing out function 
 adult_cod <- spatial_agg(gdf = dist,
-                         gdf_agg = adult_gid,
+                         agg = adult_gid,
                          mapping = mapping,
                          gdf_id = "gid", 
-                         gdf_agg_id = "gid_dist",
+                         agg_id = "gid_dist",
                          is_spatial_join = FALSE,
                          count_col = "deaths")
