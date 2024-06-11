bfa78afc5d994a86bdea9ba2728e99d4c3339400
DAlphonso <dantealphonso@gmail.com>
2024-06-11 15:49:19 -0400

    edited district_cod

--- experiments/child_malaria.R
+++ experiments/child_malaria.R
@@ -34,6 +34,97 @@ child_r2_gid <- left_join(child_r2, gid_r2, by = "geoid")
 # Combine r1 and r2 child data
 child <- bind_rows(child_r1_gid, child_r2_gid)
 
+# Get unique district names
+uniq_dname <- unique(dist$distname)
+uniq_dcod <- unique(child$district_cod)
+
+# See distname and district_cod
+cat(
+    "Before Fix\n----------\n\ndistname:\n",
+    paste0(uniq_dname, collapse = "\n "),
+    "\n\ndistrict_cod:\n",
+    paste0(uniq_dcod, collapse = "\n "),
+    "\n\n"
+)
+
+# Find all distname not in district_cod
+dname_nin_dcod <- uniq_dname[!uniq_dname %in% uniq_dcod]
+
+# Find all district_cod not in distname
+dcod_nin_dname <- uniq_dcod[!uniq_dcod %in% uniq_dname]
+
+# See conflicting distname and district_cod
+cat(
+    "\nBefore Fix\n----------\n\ndistname not in district_cod:\n",
+    paste0(dname_nin_dcod, collapse = "\n "),
+    "\n\ndistrict_cod not in distname:\n",
+    paste0(dcod_nin_dname, collapse = "\n "),
+    "\n"
+)
+
+# Match conflicts (EDIT AS NEEDED)
+# Probably a good idea to place this before joining by distname
+# THIS CODE IS PLACED IN YOUR MAIN CODE FILE TO FIX dist
+dist <- dist %>%
+    mutate(
+        distname = case_when(
+            distname == "Western Area Rur" ~ "Western Area Rural",
+            distname == "Western Area Urb" ~ "Western Area Urban",
+            .default = distname)
+    )
+
+# Get unique district names
+uniq_dname <- unique(dist$distname)
+uniq_dcod <- unique(child$district_cod)
+
+# See distname and district_cod
+cat(
+    "After Fix\n---------\n\ndistname:\n",
+    paste0(uniq_dname, collapse = "\n "),
+    "\n\ndistrict_cod:\n",
+    paste0(uniq_dcod, collapse = "\n "),
+    "\n"
+)
+
+# Find all distname not in district_cod
+dname_nin_dcod <- uniq_dname[!uniq_dname %in% uniq_dcod]
+
+# Find all district_cod not in distname
+dcod_nin_dname <- uniq_dcod[!uniq_dcod %in% uniq_dname]
+
+# See conflicting distname and district_cod
+cat(
+    "\nAfter Fix\n---------\n\ndistname not in district_cod:\n",
+    paste0(dname_nin_dcod, collapse = "\n "),
+    "\n\ndistrict_cod not in distname:\n",
+    paste0(dcod_nin_dname, collapse = "\n "),
+    "\n"
+)
+
+# Corrected district_cod to correct values
+child <- child %>%
+    mutate(district_cod = ifelse(is.na(district_cod) | district_cod == "",
+                             case_when(
+                                 gid_dist == 1 ~ "Kailahun",
+                                 gid_dist == 2 ~ "Kenema",
+                                 gid_dist == 3 ~ "Bombali",
+                                 gid_dist == 4 ~ "Kono",
+                                 gid_dist == 5 ~ "Tonkolili",
+                                 gid_dist == 6 ~ "Bo",
+                                 gid_dist == 7 ~ "Karene",
+                                 gid_dist == 8 ~ "Kambia",
+                                 gid_dist == 9 ~ "Falaba",
+                                 gid_dist == 10 ~ "Koinadugu",
+                                 gid_dist == 11 ~ "Port Loko",
+                                 gid_dist == 12 ~ "Bonthe",
+                                 gid_dist == 13 ~ "Pujehun",
+                                 gid_dist == 14 ~ "Moyamba",
+                                 gid_dist == 15 ~ "Western Area Rural",
+                                 gid_dist == 16 ~ "Western Area Urban",
+                                 TRUE ~ district_cod
+                             ),
+                             district_cod))
+
 # Created new column for child displaying final ICD-10 code cause of death
 child <- child %>% mutate_all(na_if,"") %>% 
     mutate(final_icd = case_when(!is.na(adj_icd) ~ adj_icd,  # Use adj_icd if it is not NA
@@ -71,24 +162,24 @@ mapping <- data.frame(
 male_child_malaria <- spatial_agg(gdf = dist,
                                         agg = male_child,
                                         mapping = mapping,
-                                        gdf_id = "gid", 
-                                        agg_id = "gid_dist",
+                                        gdf_id = "distname", 
+                                        agg_id = "district_cod",
                                         is_spatial_join = FALSE,
                                         count_col = "malaria_deaths")
 
 female_child_malaria <- spatial_agg(gdf = dist,
                                           agg = female_child,
                                           mapping = mapping,
-                                          gdf_id = "gid", 
-                                          agg_id = "gid_dist",
+                                          gdf_id = "distname", 
+                                          agg_id = "district_cod",
                                           is_spatial_join = FALSE,
                                           count_col = "malaria_deaths")
 
 child_agg <- spatial_agg(gdf = dist,
                          agg = child,
                          mapping = mapping,
-                         gdf_id = "gid", 
-                         agg_id = "gid_dist",
+                         gdf_id = "distname", 
+                         agg_id = "district_cod",
                          is_spatial_join = FALSE,
                          count_col = "all_deaths")
 
